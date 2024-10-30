ff95184cb15bf0ea87c0c7dcaab6990f6a048232
Grigory P <grisha9871234@gmail.com>
2024-10-30 16:12:58 +0300

    Writing new data to new JSON

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
+from json import JSONDecodeError
 from pprint import pprint
 
 class Venue:
     def __init__(self, venue_id: str, name: str, location: str, capacity: int):
+        if capacity <= 0:
+            raise ValueError("Capacity должно быть положительным целым числом.")
         self.venue_id = venue_id
         self.name = name
         self.location = location
@@ -24,6 +27,8 @@ class Customer:
 
 class Ticket:
     def __init__(self, ticket_id: str, event_id: str, seat_number: str, price: float, customer_id: str):
+        if price < 0:
+            raise ValueError("Price должна быть положительным числом.")
         self.ticket_id = ticket_id
         self.event_id = event_id
         self.seat_number = seat_number
@@ -39,6 +44,8 @@ class Booking:
 
 class Payment:
     def __init__(self, payment_id: str, booking_id: str, amount: float, payment_date: str, method: str):
+        if amount < 0:
+            raise ValueError("Amount должна быть положительным числом.")
         self.payment_id = payment_id
         self.booking_id = booking_id
         self.amount = amount
@@ -46,16 +53,23 @@ class Payment:
         self.method = method
 
 # Выгрузка инфы из старого файла
-filename = 'event.json'
+filename = 'Event.json'
 
 def load_data(filename):
-    with open('event.json', 'r', encoding='utf8') as f:
-        return json.load(f)
+    try:
+        with open(filename, 'r', encoding='utf8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("Файл не найден.")
+        return {}
+    except JSONDecodeError:
+        print("Ошибка декодирования JSON.")
+        return {}
 
 old_data = load_data(filename)
 
 # Создание объектов
-venue = Venue("V001", "Concert Hall", "New York", 5000)
+venue = Venue("V001", "Concert Hall", "New York", 6000)
 event = Event("E001", "Rock Concert", "2024-11-20", "V001")
 customer = Customer("C001", "John Doe", "john.doe@example.com", "123-456-7890")
 ticket = Ticket("T001", "E001", "A1", 100.0, "C001")
@@ -83,9 +97,19 @@ add_data(old_data, new_data)
 # Запись и чтение данных в/из JSON
 new_filename = 'Event2.json'
 
-with open(new_filename, 'w', encoding='utf8') as f:
-    json.dump(old_data, f, ensure_ascii=False, indent=2)
+try:
+    with open(new_filename, 'w', encoding='utf8') as f:
+        json.dump(old_data, f, ensure_ascii=False, indent=2)
+except IOError:
+    print("Ошибка записи в файл.")
 
-with open(new_filename, 'r', encoding='utf8') as f:
-    text = json.load(f)
-    pprint(text)
+try:
+    with open(new_filename, 'r', encoding='utf8') as f:
+        text = json.load(f)
+        pprint(text)
+except FileNotFoundError:
+    print("Файл для чтения не найден.")
+except IOError:
+    print("Ошибка чтения файла.")
+except JSONDecodeError:
+    print("Ошибка JSON.")
